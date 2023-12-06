79569d041f3f069be44efee449f3d02f720c9560
Sai Vipraj Telukoti <54499960+viprajtelukoti@users.noreply.github.com>
2023-12-06 11:20:49 -0500

    Added comments and description of changes

--- Notes_updated.py
+++ Notes_updated.py
@@ -1,3 +1,12 @@
+#Changes made
+
+#Class Structure: Introduced a class (NotesApp) to encapsulate the functionality, promoting better organization and reusability.
+#Functions: Separated various functionalities into functions to improve code modularity and readability.
+#Global Variables: Reduced the use of global variables by utilizing class attributes.
+#Consistent Naming: Ensured consistent naming conventions for variables and functions within the class.
+#Button Commands: Used instance methods as command functions for buttons, improving clarity and maintaining encapsulation
+
+
 import os
 from tkinter import *
 from tkinter.filedialog import asksaveasfilename
@@ -13,50 +22,66 @@ class NotesApp:
         self.create_gui()
 
     def create_gui(self):
-        self.main_frame = Frame(self.root, bg='#C2C0BE')
-        self.main_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
-        self.main_frame.lift()
+        # Frames
+        self.main_frame = Frame(self.root, bg='#C2C0BE')  # Main frame for note creation
+        self.show_frame = Frame(self.root, bg='#C2C0BE')  # Frame for displaying note titles
+        self.display_frame = Frame(self.root, bg='#C2C0BE')  # Frame for showing note content
 
-        self.show_frame = Frame(self.root, bg='#C2C0BE')
-        self.show_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
-        self.show_frame.lower()
-
-        self.display_frame = Frame(self.root, bg='#C2C0BE')
-        self.display_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
-        self.display_frame.lower()
-
+        # Widgets for note creation
         self.title_label = Label(self.main_frame, text="Title  :  ", font=('Calibri', 20), bg='#BEB4AA', relief=RAISED)
-        self.title_label.place(relx=0.1, rely=0.05, relwidth=0.1, relheight=0.08)
-
         self.title_var = StringVar()
         self.title_entry = Entry(self.main_frame, textvariable=self.title_var, font=('Calibri', 20), relief=RAISED)
-        self.title_entry.place(relx=0.2, rely=0.05, relwidth=0.6, relheight=0.08)
-
         self.body = Text(self.main_frame, font=('Calibri', 16), relief=RAISED)
-        self.body.place(relx=0.2, rely=0.2, relwidth=0.6, relheight=0.5)
 
+        # Buttons for note creation
         Button(self.main_frame, text='Save', font=('Calibri', 20), command=self.save).place(relx=0.4, rely=0.8, relwidth=0.2, relheight=0.1)
         Button(self.main_frame, text='Show All', font=('Calibri', 20), command=self.show_all).place(relx=0.7, rely=0.8, relwidth=0.2, relheight=0.1)
 
+        # Styling for Treeview
         style = ttk.Style()
         style.theme_use('clam')
         style.configure("mystyle.Treeview", font=('Calibri', 14), rowheight=50)
         style.configure("mystyle.Treeview.Heading", font=('Calibri', 18, 'bold'))
 
+        # Treeview for displaying note titles
         self.tree = ttk.Treeview(self.show_frame, selectmode='browse', style="mystyle.Treeview", show=['headings'])
         self.tree['columns'] = ('0')
         self.tree.heading("0", text="Title")
         self.tree.column("0", minwidth=100, width=275)
-        self.tree.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.7)
         self.tree.bind("<Double-1>", self.on_double_click)
 
+        # Buttons for note deletion
         Button(self.main_frame, text='Delete', font=('Calibri', 20), command=self.delete_note).place(relx=0.1, rely=0.8, relwidth=0.2, relheight=0.1)
 
+        # ScrolledText for displaying note content
         self.dis_label = tkscroll.ScrolledText(self.display_frame, font=('Calibri', 20), bg='#ffffff', relief=GROOVE)
-        self.dis_label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.7)
 
+        # Buttons for navigating between frames
         Button(self.show_frame, text='Back', font=('Calibri', 20), command=lambda: self.back('show')).place(relx=0.4, rely=0.85, relwidth=0.2, relheight=0.1)
         Button(self.display_frame, text='Back', font=('Calibri', 20), command=lambda: self.back('display')).place(relx=0.4, rely=0.85, relwidth=0.2, relheight=0.1)
+
+        # Placing widgets and frames
+        self.place_widgets()
+        
+    def place_widgets(self):
+        # Note Creation Widgets
+        self.title_label.place(relx=0.1, rely=0.05, relwidth=0.1, relheight=0.08)
+        self.title_entry.place(relx=0.2, rely=0.05, relwidth=0.6, relheight=0.08)
+        self.body.place(relx=0.2, rely=0.2, relwidth=0.6, relheight=0.5)
+
+        # Note Display Widgets
+        self.dis_label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.7)
+
+        # Placing frames
+        self.main_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
+        self.main_frame.lift()
+        self.show_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
+        self.show_frame.lower()
+        self.display_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
+        self.display_frame.lower()
+
+        # Treeview
+        self.tree.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.7)
 
     def back(self, frame):
         if frame == 'show':
